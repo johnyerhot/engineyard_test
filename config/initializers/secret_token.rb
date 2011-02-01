e73071fd9757ae1b1c6a48492babba2ac525cec4
John Yerhot <jyerhot@engineyard.com>
2011-02-01 09:47:17 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestEy::Application.config.secret_token = '80b860ac7f4c011a8466e42b425478624fc01598cc7f301310012e6ce92620997f0f4acb062b1987114a0feb9d80ce9f4d16d94bebc6dd8f001e66361958b378'
